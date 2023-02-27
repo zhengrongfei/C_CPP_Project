18f1b11ebf8d449d1a210c0b163c066dd8eb4b27
zhengrongfei <105427287+zhengrongfei@users.noreply.github.com>
2023-02-27 22:56:42 +0800

    Update main.c
    添加了乘法运算功能

--- Project1/main.c
+++ Project1/main.c
@@ -3,21 +3,31 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define SIGNIFICANTDIGITS  20
+#define SIGNIFICANTDIGITS  100
+enum OPERATOR
+{
+      ADD=1, SUBSTRUCT, MUL, DIV
+};
+
+
 struct NUMBER{
     int x ; //10以内的数字
     int exp; 
     int mantissa[SIGNIFICANTDIGITS-1]; //位数：significantDigits-1
+    int countNumber;
 };
 //把字符串转化为int array
  struct NUMBER parseNumber(char num[],struct NUMBER res,int size){
     int begin=0;
     res.exp=0;
-    while(num[begin]=='0'){
-        begin++;
-        res.exp--;
+    if(num[0]=='0'){
+        begin=2;
+        res.exp=-1;
+        while(num[begin]=='0'){
+            begin++;
+            res.exp--;
+        }
     }
-    printf("exp:%d",res.exp);
     for(int i=0;i<SIGNIFICANTDIGITS-1;i++){
         res.mantissa[i]=0;
     }
@@ -44,18 +54,14 @@ struct NUMBER{
             }
             else{
                 exp=num[i+1]-'0';
-                printf("num[%d]=%d:",i+1,num[i+1]-'0');
             }
             i++;
             while(i<size-1){
                 exp=exp*10+num[i+1]-'0';
-                printf("num[%d]=%d:",i+1,num[i+1]-'0');
                 i++;
             }
             exp=exp*sign;
             res.exp=res.exp+exp;
-            printf("exp:%d",exp);
-            printf("exp:%d",res.exp);
             break;
         }
         else if(num[i]=='.'){
@@ -64,7 +70,7 @@ struct NUMBER{
     }
 
     res.exp=res.exp+e;
-    printf("exp:%d",res.exp);
+    res.countNumber=coutNumber;
     return res;
 }
 
@@ -78,25 +84,105 @@ int get_length(char str[])
 	}
 	return count;
 }
+int chooseOperator(char symble[]){
+    switch (symble[0])
+    {
+    case '+':
+        return 1;
+    case '-':
+        return 2;
+    case '/':
+        return 4;
+    default:
+        return 3;
+    }
+    return 0;
+}
 
+
+void mul(struct NUMBER num1,struct NUMBER num2){
+    //乘法不用对齐
+
+    char ans[SIGNIFICANTDIGITS*2]={0};
+    for(int i=num2.countNumber-1;i>=-1;i--){
+        for(int j=num1.countNumber-1;j>=-1;j--){
+            int b=num1.mantissa[j];
+            int a=num2.mantissa[i];
+            if(j==-1){
+                b=num1.x;
+            }
+            if(i==-1){
+                a=num2.x;
+            }
+
+            int tmp=a*b;
+            ans[num2.countNumber+num1.countNumber-2-i-j]+=tmp;
+            //进位处理
+            ans[num2.countNumber+num1.countNumber-1-i-j]+=(ans[num2.countNumber+num1.countNumber-2-i-j]/10);
+            ans[num2.countNumber+num1.countNumber-2-i-j]=ans[num2.countNumber+num1.countNumber-2-i-j]%10;
+        }
+    }
+    int exp=num1.exp+num2.exp;
+    //判断x有没有变成两位
+    if(ans[num1.countNumber+num2.countNumber+1]!=0){
+        exp=exp+1;
+    }
+    //输出结果
+    for(int i=num1.countNumber+num2.countNumber+1;i>=0;i--){
+        if((num1.x==0||num2.x==0)){
+            return;
+        }
+        else if(i==num1.countNumber+num2.countNumber+1&&ans[num1.countNumber+num2.countNumber+1]==0){
+            continue;
+        }
+        printf("%d",ans[i]);
+        if(i==num1.countNumber+num2.countNumber+1 && ans[i]!=0){
+            printf(".");
+        }
+    }
+    printf("e%d\n",exp);
+}
 
 int main(int argc, char* argv[]) {
-    // 创建一个字符串数组
-    char num[SIGNIFICANTDIGITS+2] = {0};
-// 创建一个指针，并指向静态区存放的一个字符串“ABC”
-    char* tmp = argv[1];
-    printf("length:%d\n",get_length(argv[1]));
-    // 利用字符串拷贝函数进行拷贝
-    strcpy(num, tmp);
-    printf("arr%s\n",num);
-// 例如内存拷贝函数进行拷贝
-    memcpy(num, tmp, sizeof(num));
-    int length=get_length(argv[1]);
+
+
+    char* o=argv[2];
+    char opt[1];
+    char* tmp1 = argv[2];
+    memcpy(opt, tmp1, 1);
+    int operator=chooseOperator(opt);
+
+    char numA[SIGNIFICANTDIGITS+2] = {0};
+    char* tmpA = argv[1];
+    strcpy(numA, tmpA);
+    memcpy(numA, tmpA, sizeof(numA));
+    int lengthA=get_length(argv[1]);
+
     struct NUMBER num1;
-    num1=parseNumber(num,num1,length);
-    printf("x%d\n", num1.x);
-    for(int i=0;i<10;i++){
-        printf("%d\n",num1.mantissa[i]);
+    num1=parseNumber(numA,num1,lengthA);
+
+    char numB[SIGNIFICANTDIGITS+2] = {0};
+    char* tmpB = argv[3];
+
+    strcpy(numB, tmpB);
+    memcpy(numB, tmpB, sizeof(numB));
+    int lengthB=get_length(argv[3]);
+
+    struct NUMBER num2;
+    num2=parseNumber(numB,num2,lengthB);
+
+
+
+
+    switch (operator)
+    {
+    case MUL:
+        /* code */
+        mul(num1,num2);
+        break;
+    default:
+        break;
     }
-    printf("exp: %d\n",num1.exp);
+    return 0;
 }
+
